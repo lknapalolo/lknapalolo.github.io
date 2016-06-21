83e4cd597c8ece3df1f1ebf3ef96609ae44a46e9
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-06-21 13:42:50 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+$('document').ready(function(){
+
+$('.text').hide();
+// $('.about-page').hide();
+
+$('body').on('mouseleave', function(){
+  $('.text').hide();
+  $('.projects').css('background-color', 'rgba(184, 208, 215, 0.3)');
+  $('.about').css('background-color', 'rgba(157,140,149,0.3)');
+})
+
+$('.about').on('mouseenter', function(){
+  $('.text').eq(0).show();
+  $('.text').eq(1).hide();
+  $('.about').css('background-color', 'rgba(184, 208, 215, 0.3)');
+  $('.projects').css('background-color', 'rgba(157,140,149,0.3)')
+})
+
+$('.projects').on('mouseenter', function(){
+  $('.text').eq(1).show();
+  $('.text').eq(0).hide();
+  $('.projects').css('background-color', 'rgba(184, 208, 215, 0.3)');
+  $('.about').css('background-color', 'rgba(157,140,149,0.3)')
+})
+
+$('.about').click(function(){
+  $('.about-page').show();
+  $('.about').hide();
+  $('.projects').hide();
+})
+
+$('.return-home').click(function(){
+  $('.about-page').hide();
+  $('.about').show();
+  $('.projects').show();
+})
+
+});
