9c439eec5681548c7e8337a8266fee1d28fea9e1
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-06-21 15:49:23 -0400

    project section template

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $('document').ready(function(){
 
 $('.text').hide();
 $('.about-page').hide();
+$('.view-projects').hide();
 
 $('body').on('mouseleave', function(){
   $('.text').hide();
@@ -13,14 +14,14 @@ $('.about').on('mouseenter', function(){
   $('.text').eq(0).show();
   $('.text').eq(1).hide();
   $('.about').css('background-color', 'rgba(184, 208, 215, 0.3)');
-  $('.projects').css('background-color', 'rgba(157,140,149,0.3)')
+  $('.projects').css('background-color', 'rgba(157,140,149,0.3)');
 })
 
 $('.projects').on('mouseenter', function(){
   $('.text').eq(1).show();
   $('.text').eq(0).hide();
   $('.projects').css('background-color', 'rgba(184, 208, 215, 0.3)');
-  $('.about').css('background-color', 'rgba(157,140,149,0.3)')
+  $('.about').css('background-color', 'rgba(157,140,149,0.3)');
 })
 
 $('.about').click(function(){
@@ -29,10 +30,18 @@ $('.about').click(function(){
   $('.projects').hide();
 })
 
+$('.projects').click(function(){
+  $('.view-projects').show();
+  $('.about').hide();
+  $('.projects').hide();
+})
+
 $('.return-home').click(function(){
+  $('.view-projects').hide();
   $('.about-page').hide();
   $('.about').show();
   $('.projects').show();
 })
+
 
 });
